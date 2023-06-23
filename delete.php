15c0637e3a352a48c6a3187aedb68620d2f71e4d
Aarik Mhrzn <mhrznaarik870@gmail.com>
2023-06-23 09:29:14 +0545

    minor-changes in delete.php

--- delete.php
+++ delete.php
@@ -1,38 +1,6 @@
 <?php
 include 'backend/_dbconnect.php';
-include 'backend/navbar.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if confirmation button is clicked
-    if (isset($_POST['confirm'])) {
-        $empid = $_POST['empid'];
-        $resignedComments = $_POST['resigned_comments'];
-
-        // Retrieve employee details from empdetails table
-        $stmt = $conn->prepare("SELECT * FROM empdetails WHERE empid = ?");
-        $stmt->bind_param("i", $empid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-
-        // Insert employee details into resigned_employees table
-        $stmt = $conn->prepare("INSERT INTO resigned_employees (empid, name, address, phoneno, email, department, resigned_comments, Reg_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("isssssss", $row['empid'], $row['name'], $row['address'], $row['phoneno'], $row['email'], $row['department'], $resignedComments, $row['Reg_date']);
-        $stmt->execute();
-
-        // Delete employee from empdetails table
-        $stmt = $conn->prepare("DELETE FROM empdetails WHERE empid = ?");
-        $stmt->bind_param("i", $empid);
-        $stmt->execute();
-
-        $message = "Employee deleted successfully!";
-        header("Location: resignedEmployees.php");
-    } else {
-        // Cancellation button is clicked, redirect back to the employee list page
-        header("Location: employee_list.php");
-        exit();
-    }
-}
+require_once 'backend/navbar.php';
 
 // Retrieve data from the database based on the empid
 if (isset($_GET['empid'])) {
@@ -56,15 +24,7 @@ if (isset($_GET['empid'])) {
 
 <body>
     <div class="container text-center col-md-9">
-
-        <?php if (isset($message)): ?>
-            <div class="alert alert-success" role="alert">
-                <?php echo $message; ?>
-            </div>
-        <?php endif; ?>
-
         <h2>Delete Employee</h2>
-
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
@@ -96,8 +56,7 @@ if (isset($_GET['empid'])) {
                                 <?php echo $row['department']; ?>
                             </li>
                             <li class="list-group-item">
-                                <form action="" method="POST"
-                                    onsubmit="return confirm('Are you sure you want to delete this employee?');">
+                                <form action="" method="POST" onsubmit="return confirmDelete();">
                                     <input type="hidden" name="empid" value="<?php echo $row['empid']; ?>">
                                     <input type="text" name="resigned_comments" placeholder="Resigned Comments">
                                     <button type="submit" name="confirm" class="btn btn-danger">Confirm</button>
@@ -110,6 +69,35 @@ if (isset($_GET['empid'])) {
             </div>
         </div>
     </div>
+
+    <script>
+        function confirmDelete() {
+            return confirm('Are you sure you want to delete this employee?');
+        }
+    </script>
+
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm'])): ?>
+        <?php
+        $empid = $_POST['empid'];
+        $resignedComments = $_POST['resigned_comments'];
+
+        // Insert employee details into resigned_employees table
+        $stmt = $conn->prepare("INSERT INTO resigned_employees (empid, name, address, phoneno, email, department, resigned_comments, Reg_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("isssssss", $row['empid'], $row['name'], $row['address'], $row['phoneno'], $row['email'], $row['department'], $resignedComments, $row['Reg_date']);
+        $stmt->execute();
+
+        // Delete employee from empdetails table
+        $stmt = $conn->prepare("DELETE FROM empdetails WHERE empid = ?");
+        $stmt->bind_param("i", $empid);
+        $stmt->execute();
+
+        $message = "Employee deleted successfully!";
+        ?>
+        <script>
+            alert("<?php echo $message; ?>");
+            window.location.href = "resignedEmployees.php";
+        </script>
+    <?php endif; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
